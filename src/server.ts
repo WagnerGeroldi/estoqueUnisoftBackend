6a64a7e088ce8637379c9e1f43e1ab5697965c5d
codesystemweb <codesystemweb@gmail.com>
2026-08-19 17:19:57 -0300

    ajustei api para dentro do src

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,15 @@ import cors from "cors";
 import "dotenv/config";
 
 const app = express();
-app.use(cors({
-  origin: ["http://localhost:3000", "https://estoque-unisoft-frontend.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "x-access-token"]
-}));
+
+// app.use(cors({
+//   origin: ["http://localhost:3000", "https://estoque-unisoft-frontend.vercel.app"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "x-access-token"]
+// }));
+
+
+app.use(cors());
 app.options("*", cors());
 app.use(express.json());
 
